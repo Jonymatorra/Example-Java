8459390bf459214d8c76c870a2ac03e1dd5dc01d
Jonymatorra <101931411+Jonymatorra@users.noreply.github.com>
2023-04-03 23:20:51 -0300

    Add files via upload

---
+++ Tipos.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        int variable1 = 50;
+        long variable2 =  20;
+        double variable3 = 2.36;
+        boolean variable4 = false;
+        String variable5 = "Hola mundo";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        
+
+    }
+}
