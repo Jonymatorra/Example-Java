6c9d74f8faece4ad76e54b5130c66324964653bc
Jonymatorra <101931411+Jonymatorra@users.noreply.github.com>
2023-05-07 14:52:28 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] frase = {"Hola", "mundo","estoy","aca"};
+
+        for (int i = 0; i < frase.length ; i++){
+            System.out.println(frase[i]);
+        }
+
+        
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
